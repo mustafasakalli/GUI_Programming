562501b09ef235118a56c7e7e6e3ceef239275ba
mustafasakalli <mustafasakalli42@gmail.com>
2019-10-31 15:16:19 +0300

    week 7 added

--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.tictactoe;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -35,6 +38,17 @@ public class MainActivity extends AppCompatActivity {
                 Button btn = (Button) row.getChildAt(j);
                 btn.setOnClickListener(new CellListener(i,j));
             }
+        }
+
+        if (savedInstanceState != null){
+            player1Turn = savedInstanceState.getBoolean("turn");
+            byte[] arr = savedInstanceState.getByteArray("board");
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    board[i][j] = arr[i*3 + j]  ;
+                }
+            }
+
         }
     }
 
@@ -169,8 +183,31 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    
-	public boolean newGame(MenuItem item) {
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putByteArray("board", toArray(board));
+        outState.putBoolean("turn", player1Turn);
+
+    }
+
+    private byte[] toArray(byte[][] matrix ){
+        int row = matrix.length;
+        int col = matrix[0]. length;
+
+        byte[] arr = new byte[row*col];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i*3 + j] = board[i][j] ;
+            }
+        }
+
+        return  arr;
+
+    }
+
+    public boolean newGame(MenuItem item) {
         setBoardEnabled(true);
 
         for (int i = 0; i < 3; i++) {
@@ -189,6 +226,57 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    public boolean saveGame(MenuItem item) {
+        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        long b = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                b += (long) (board[i][j] * Math.pow(10,i*3 + j));
+            }
+        }
+
+        editor.putLong("board", b);
+        editor.putBoolean("turn", player1Turn);
+        editor.commit();
+
+        return true;
+
+    }
+    public boolean laodGame(MenuItem item) {
+        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+        player1Turn = preferences.getBoolean("turn", true);
+        long b = preferences.getLong("board", 0);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                board[i][j] = (byte) ((b/Math. pow(10,i*3 + j)) % 10);
+            }
+        }
+
+        TableLayout table = findViewById(R.id.table);
+        for (int i = 0, i < 3, i++){
+            TableRow row = (TableRow) table.getChildAt(i);
+            for (int j = 0; j < 3; j++){
+                Button btn = (Button) row.getChildAt(j);
+                switch (board[i][j]){
+                    case 0: btn.SetText("");
+                    break;
+                    case 1: btn.setText(PLAYER_1_SYMBOL);
+                        break;
+                    case 2:
+                }
+            }
+        }
+
+        return true;
+
+
     }
 
 
